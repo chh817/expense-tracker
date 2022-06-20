6b55a5e06e509bd4187f53c0999bf77fd2df37c6
chh817 <chh817@gmail.com>
2022-06-20 21:11:39 +0800

    feat: add dotenv

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const express = require('express')
 const app = express()
 const port = 3000 || process.env.PORT
-
+if (process.env.NOD_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 // Requiring mongoose
 require("./config/mongoose")
