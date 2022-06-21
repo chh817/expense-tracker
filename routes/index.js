53bb883fd84f6b966f67249da562e4d6e0f5b79c
chh817 <chh817@gmail.com>
2022-06-21 14:05:08 +0800

    feat: add records.js

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@
 const express = require('express')
 const router = express.Router()
 const users = require('./modules/users')
+const records = require('./modules/records')
 const home = require('./modules/home')
 
 // Guiding request into route
 router.use('/users', users)
+router.use('/records', records)
 router.use('/', home)
 
 module.exports = router
