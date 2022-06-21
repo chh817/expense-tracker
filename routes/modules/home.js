26570409b88825013a81c55e609ef2da4e47d05a
chh817 <chh817@gmail.com>
2022-06-21 20:58:35 +0800

    feat: modify home.js & index.hbs

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,16 +2,29 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 // Route for index page
 router.get('/', (req, res) => {
   const userId = req.user._id
-  Record.find({ userId })
-    .sort('date')
+  const select = req.query.select || '全部'
+  let totalAmount = 0
+  Record.find({ userId, select })
+    .sort(select)
     .lean()
-    .then(records => res.render('index', { records }))
+    .then(records => {
+      Promise.all(Array.from(records, record => {
+        totalAmount += record.amount
+        Category.findOne({ select })
+          .lean()
+          .then(category => {
+            const categories = []
+            categories.push(category)
+          })
+      }))
+      return res.render('index', { totalAmount, records, categories })
+    })
     .catch(err => console.error(err))
 })
-
 
 module.exports = router
