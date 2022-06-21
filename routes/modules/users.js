72ba13e9c4fd77194c7285fe0e0007b43aa5c993
chh817 <chh817@gmail.com>
2022-06-21 17:08:09 +0800

    feat: add password hashing feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 // Route for login page
 router.get('/login', (req, res) => res.render('login'))
